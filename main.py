350ae892bdffcceb5840863a8238fc7045b41507
Oussama <88498674+oussamachaib@users.noreply.github.com>
2023-05-01 11:28:00 +0100

    corrections

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ import time
 client = tweepy.Client(bearer_token, api_key, api_key_secret, access_token, access_token_secret)
 auth = tweepy.OAuth1UserHandler(api_key, api_key_secret,access_token, access_token_secret)
 
-client.create_tweet(text = 'This is a test.')
+client.create_tweet(text = 'This is a second test.')
